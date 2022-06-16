3e00ce06e5c1179ca0cd6d5ff366c1a2ad9de72f
jaemoon <jaemoon406@gmail.com>
2022-06-16 23:27:57 +0900

    로그인, 회원가입 예외처리(api 완료)

--- apps/auth/serializers.py
+++ apps/auth/serializers.py
@@ -12,32 +12,3 @@ class SignUpSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True)
 
-    def create(self, validated_data):
-        user = User.objects.create(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            nickname=validated_data['username']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-
-class SignInSerializer(serializers.ModelSerializer):
-    model = User
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True)
-
-    def post(self, data):
-        user = User.objects.get(username=data['username'])
-        user.check_password(data['password'])
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'nickname', 'profile_image', 'email', 'date_joined']
-        # fields = '__all__'
-# def post(self, data):
-#     user = User.objects.get(username=data['username'])
-#     user.check_password(data['password'])
