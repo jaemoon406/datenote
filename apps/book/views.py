7b262f07b60cbe144b728ea02cd766a3eb450014
jaemoon <jaemoon406@gmail.com>
2022-08-31 18:59:37 +0900

    [Modify] BookViewSet 수정

--- apps/book/views.py
+++ apps/book/views.py
@@ -1,6 +1,5 @@
 import os
 
-from apps.util.encryption import AESCipher
 from django.contrib.auth import get_user_model
 
 from rest_framework.decorators import api_view, permission_classes
@@ -10,52 +9,41 @@ from rest_framework.viewsets import ViewSet, ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework.renderers import JSONRenderer
-from rest_framework.pagination import PageNumberPagination
 
-from apps.util.views import query_debugger
 from .serializers import BookListSerializer, BookSerializer
+from apps.util.views import query_debugger
 from apps.book.models import Book, BookMember
+from apps.util.encryption import AESCipher
 from apps.util.json_response import *
+from apps.util.paginations import ListPagination
+
 
 User = get_user_model()
 aes = AESCipher(key=os.environ.get('PRIVATE_KEY'))
 
-
-class ListPagination(PageNumberPagination):
-    page_size = 5
-
-
-class Board(ViewSet):
-    def get_view_name(self):
-        return
-
-
 class BookViewSet(ModelViewSet):
     # permission_classes = [IsAuthenticated]
     permission_classes = [AllowAny]
-    queryset = Book.objects.all()
+    queryset = Book.objects.all().order_by('-created')
     serializer_class = BookSerializer
     renderer_classes = [JSONRenderer]
     pagination_class = ListPagination
 
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-        return self.get_paginated_response(serializer.data)
+        self.queryset = Book.objects.filter(is_public=False)
+        return super().list(self, request, *args, **kwargs)
 
     def retrieve(self, request, pk, *args, **kwargs):
-        instance = self.get_object()
-        serializer = BookListSerializer(instance)
-        return Response(serializer.data)
+        return super().retrieve(self, request, pk, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
         if request.user:
             data = request.data
             data['owner'] = request.user
             data['password'] = aes.encrypt(data['password'])
+            request = request
             BookSerializer.create(self, validated_data=data)
-            return Response(json_success('S0001', {"Success"}), status=status.HTTP_201_CREATED)
+            return super().create(self, request, *args, **kwargs)
+            # return Response(json_success('S0001', {"Success"}), status=status.HTTP_201_CREATED)
         else:
             return Response(json_error('E0403'), status=status.HTTP_200_OK)
